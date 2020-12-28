baf1520c00b89930e79a7ab7b6ef657f1826ebc1
Rustam <48557727+RustamRuslanovich@users.noreply.github.com>
2020-12-28 14:09:32 +0300

    Add files via upload

--- test_starter.py
+++ test_starter.py
@@ -2,18 +2,16 @@ from __future__ import unicode_literals
 import unittest
 import jetproj.fitting_master as fitting_master
 import numpy as np
-# import matplotlib.pyplot as plt
-# import os
-# import emcee
+import sys
+
 
 class StarterTest(unittest.TestCase):
     def test_timing(self):
-        self.assertTrue(np.array_equal(fitting_master.open_time2(path='../'), fitting_master.open_time8('../')),
-                        msg='epochs are not synchronized')
+        np.testing.assert_array_equal(fitting_master.open_time2('../'), fitting_master.open_time8('../'))
 
     def test_flux(self):
-        self.assertTrue((False == (fitting_master.open_fl1(path='../') < 0)).any(), msg='you have a negative flux in fl1')
-        self.assertTrue((False == (fitting_master.open_fl2(path='../') < 0)).any(), msg='you have a negative flux in fl2')
+        self.assertTrue(np.any(fitting_master.open_fl1(path='../') >= 0), msg='you have a negative flux in fl1')
+        self.assertTrue(np.any(fitting_master.open_fl2(path='../') >= 0), msg='you have a negative flux in fl2')
 
 
 if __name__ == '__main__':
